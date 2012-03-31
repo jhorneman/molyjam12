988e6438b68a8acd39a3a5a47b577c401fa09e4e
Jurie Horneman <jhorneman@pobox.com>
2012-03-31 09:59:11 +0200

    Forgot to stage these files

--- data.py
+++ data.py
@@ -1,1 +1,38 @@
+import sys
+import os
+import os.path
+import markdown
+import re
+
 __author__ = 'jhorneman'
+
+SCRIPT_DIR = os.path.abspath(os.path.dirname(sys.argv[0])) + os.sep
+
+scenes = {}
+scene_files = []
+
+def load_data():
+    for path, dirs, files in os.walk(os.path.join(SCRIPT_DIR, "scenes")):
+        for filename in files:
+            if filename.startswith("."):
+                continue
+            full_path = os.path.join(path, filename)
+            scene_name = os.path.splitext(filename)[0]
+            with open(full_path, "r") as f:
+                d = f.read()
+                d = d.strip()
+                if '------' in d:
+                    text, option_string = d.split('------')
+                    options = parse_options(option_string)
+                else:
+                    text = d
+                    options = []
+                html = markdown.markdown(text)
+                scenes[scene_name] = (html, options)
+                scene_files.append(full_path)
+
+def parse_options(_option_string):
+    options = []
+    for option in re.findall(r'\[(.*)\](.*)', _option_string, re.MULTILINE):
+        options.append(option)
+    return options
--- main.py
+++ main.py
@@ -1,1 +1,49 @@
 __author__ = 'jhorneman'
+
+from flask import Flask, session, redirect, url_for, escape, request, render_template
+from data import load_data, scenes, scene_files
+
+app = Flask(__name__)
+
+user_data = {}
+
+@app.route("/")
+def index():
+    if 'username' in session:
+        return redirect(url_for('scene'))
+    return "You are not logged in"
+
+@app.route("/scene/")
+@app.route("/scene/<scene_name>")
+def scene(scene_name='start'):
+    return render_template('scene.html', description=scenes[scene_name][0], options=scenes[scene_name][1])
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if its there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'n\x86\xd4\x85\xcevqe\x82\xc1K\xc6|\x00\x0fl\xcf\x16\t\x9a\xa0QB\xf2'
+
+if __name__ == "__main__":
+    load_data()
+    app.run(debug=True, extra_files=scene_files)
